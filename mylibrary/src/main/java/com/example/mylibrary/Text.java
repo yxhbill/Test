f6b839e70894809e52640d1b382cdc12ef5dcab8
jlyangxuehao <927095748@qq.com>
2018-11-29 15:34:02 +0800

    第一次

---
+++ mylibrary/src/main/java/com/example/mylibrary/Text.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * Created by jlyangxuehao on 2018/11/29.
+ */
+
+public class Text {
+    public  int addSum(int a,int b){
+        return  a + b ;
+    }
+}
